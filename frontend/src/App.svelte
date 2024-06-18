407cf5036a58badfcc7f5a51b5999c3773d6a9d9
HeoYM <dudan0123@gmail.com>
2024-06-18 20:04:21 +0900

    Initial commit - pybo base

---
+++ frontend/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Router from 'svelte-spa-router'
+  import Home from "./routes/Home.svelte"
+  import Detail from "./routes/Detail.svelte"
+  import QuestionCreate from './routes/QuestionCreate.svelte';
+
+  const routes = {
+    '/' : Home,
+    '/question-create' : QuestionCreate,
+    '/detail/:question_id' : Detail,
+    
+  }
+</script>
+
+<Router {routes}/>
