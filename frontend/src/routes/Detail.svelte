c07371b7589fdacecad73329ca12c35af6a01e60
HeoYM <dudan0123@gmail.com>
2024-06-19 17:13:11 +0900

    add navbar pagination store변수

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -1,50 +1,52 @@
 <script>
-    import fastapi from "../lib/api"
-    import Error from "../components/Error.svelte"
+    import fastapi from "../lib/api";
+    import Error from "../components/Error.svelte";
+    import { push } from 'svelte-spa-router'
 
-    export let params = {}
-    let question_id = params.question_id
-    let question = {answers:[]}
-    let content = ""
-    let error = {detail:[]}
+    export let params = {};
+    let question_id = params.question_id;
+    let question = { answers: [] };
+    let content = "";
+    let error = { detail: [] };
 
-
-
-    function get_question(){
+    function get_question() {
         fastapi("get", "/api/question/detail/" + question_id, {}, (json) => {
-            question = json
-        })
+            question = json;
+        });
     }
 
-    get_question()
-
+    get_question();
 
     function post_answer(event) {
-        event.preventDefault()
-        let url = "/api/answer/create/" + question_id
+        event.preventDefault();
+        let url = "/api/answer/create/" + question_id;
         let params = {
-            content: content
-        }
-        fastapi('post', url, params, 
+            content: content,
+        };
+        fastapi(
+            "post",
+            url,
+            params,
             (json) => {
-                content = ''
-                error = {detail:[]}
-                get_question()
+                content = "";
+                error = { detail: [] };
+                get_question();
             },
             (err_json) => {
-                error = err_json
-            }
-        )
+                error = err_json;
+            },
+        );
     }
 </script>
-
 
 <div class="container my-3">
     <!-- 질문 -->
     <h2 class="border-bottom py-2">{question.subject}</h2>
     <div class="card my-3">
         <div class="card-body">
-            <div class="card-text" style="white-space: pre-line;">{question.content}</div>
+            <div class="card-text" style="white-space: pre-line;">
+                {question.content}
+            </div>
             <div class="d-flex justify-content-end">
                 <div class="badge bg-light text-dark p-2">
                     {question.create_date}
@@ -52,26 +54,46 @@
             </div>
         </div>
     </div>
-    <!-- 답변 목록 -->
-    <h5 class="border-bottom my-3 py-2">{question.answers.length}개의 답변이 있습니다.</h5>
-    {#each question.answers as answer}
-    <div class="card my-3">
-        <div class="card-body">
-            <div class="card-text" style="white-space: pre-line;">{answer.content}</div>
-            <div class="d-flex justify-content-end">
-                <div class="badge bg-light text-dark p-2">
-                    {answer.create_date}
+
+    <div class="container">
+        <!-- 답변 목록 -->
+        <h5 class="border-bottom my-3 py-2">
+            {question.answers.length}개의 답변이 있습니다.
+        </h5>
+        {#each question.answers as answer}
+            <div class="card my-3">
+                <div class="card-body">
+                    <div class="card-text" style="white-space: pre-line;">
+                        {answer.content}
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <div class="badge bg-light text-dark p-2">
+                            {answer.create_date}
+                        </div>
+                    </div>
                 </div>
             </div>
+        {/each}
+        
+        <!-- 답변 등록 -->
+        <div class="my-3">
+            <Error {error} />
+            <form method="post">
+                <div class="mb-3">
+                    <textarea rows="10" bind:value={content} class="form-control"></textarea>
+                </div>
+                
+                <div class="row align-items-center">
+                    <div class="col">
+                        <button type="button" class="btn btn-secondary" on:click={() => push('/')}>
+                            목록으로
+                        </button>
+                    </div>
+                    <div class="col-auto">
+                        <input type="submit" value="답변등록" class="btn btn-primary" on:click={post_answer} />
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-    {/each}
-    <!-- 답변 등록 -->
-    <Error error={error} />
-    <form method="post" class="my-3">
-        <div class="mb-3">
-            <textarea rows="10" bind:value={content} class="form-control" />
-        </div>
-        <input type="submit" value="답변등록" class="btn btn-primary" on:click="{post_answer}" />
-    </form>
 </div>
--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,39 +1,108 @@
 <script>
-  import fastapi from "../lib/api"
-  import { link } from 'svelte-spa-router'
+    import fastapi from "../lib/api"
+    import { link } from "svelte-spa-router"
+    import { page } from "../lib/store"
+    
+    let question_list = []
+    let size = 10;
+    let total = 0;
+    // svelte구문 $사용하면 종속된 변수가 변할때마다 total_page 갱신
+    $: total_page = Math.ceil(total/size);
 
 
-  let question_list = []
+    function get_question_list(_page) {
+        let params = {
+            page : _page,
+            size : size,
+        }
+        fastapi("get", "/api/question/list", params, (json) => {
+            question_list = json.question_list,
+            $page = _page,
+            total = json.total
+        });
+    }
 
-  function get_question_list() {
-      fastapi('get', '/api/question/list', {}, (json) => {
-          question_list = json
-      })
-  }
-
-  get_question_list()
+    $: get_question_list($page);
 </script>
 
 <div class="container my-3">
-  <table class="table">
-      <thead>
-      <tr class="table-dark">
-          <th>번호</th>
-          <th>제목</th>
-          <th>작성일시</th>
-      </tr>
-      </thead>
-      <tbody>
-      {#each question_list as question, i}
-      <tr>
-          <td>{i+1}</td>
-          <td>
-              <a use:link href="/detail/{question.id}">{question.subject}</a>
-          </td>
-          <td>{question.create_date}</td>
-      </tr>
-      {/each}
-      </tbody>
-  </table>
+    
     <a use:link href="/question-create" class="btn btn-primary">질문 등록하기</a>
+    
+    <table class="table">
+        <thead>
+            <tr class="table-dark">
+                <th>번호</th>
+                <th>제목</th>
+                <th>작성일시</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each question_list as question, i}
+                <tr>
+                    <td>{i + 1}</td>
+                    <td>
+                        <a use:link href="/detail/{question.id}"
+                            >{question.subject}</a
+                        >
+                    </td>
+                    <td>{question.create_date}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+    
+    <!-- 페이징처리 시작 -->
+    <ul class="pagination pagination-sm justify-content-center">
+        <!-- 이전페이지 -->
+        <li class="page-item {$page <= 0 && 'disabled'}">
+            <a class="page-link" href="#" aria-label="Previous" on:click="{() => get_question_list($page-1)}">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+
+    <!-- 페이지번호 -->
+    {#if total_page <= 9}
+        {#each Array(total_page) as _, loop_page}
+            <li class="page-item {loop_page === $page && 'active'}">
+                <a href="#" class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page + 1}</a>
+            </li>
+        {/each}
+    {:else}
+        {#if $page <= 5}
+            {#each Array(9) as _, loop_page}
+                <li class="page-item {loop_page === $page && 'active'}">
+                    <a href="#" class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page + 1}</a>
+                </li>
+            {/each}
+            <li class="page-item disabled"><span class="page-link">...</span></li>
+        {:else if $page >= total_page - 4}
+            <li class="page-item disabled"><span class="page-link">...</span></li>
+            {#each Array(9) as _, loop_page}
+                <li class="page-item {total_page - 9 + loop_page === $page && 'active'}">
+                    <a href="#" class="page-link" on:click="{() => get_question_list(total_page - 9 + loop_page)}">{total_page - 9 + loop_page + 1}</a>
+                </li>
+            {/each}
+        {:else}
+            {#each Array(9) as _, loop_page}
+                <li class="page-item {$page - 4 + loop_page === $page && 'active'}">
+                    <a href="#" class="page-link" on:click="{() => get_question_list($page - 4 + loop_page)}">{$page - 4 + loop_page + 1}</a>
+                </li>
+            {/each}
+            <li class="page-item disabled"><span class="page-link">...</span></li>
+        {/if}
+    {/if}
+
+    <!-- 다음페이지 -->
+    <li class="page-item {$page >= total_page - 1 && 'disabled'}">
+        <a class="page-link" href="#" aria-label="Next" on:click="{() => get_question_list($page + 1)}">
+            <span aria-hidden="true">&raquo;</span>
+        </a>
+    </li>
+    <!-- 페이징처리 끝 -->
+    
+    </ul>
+
+
 </div>
+
